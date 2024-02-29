212997e8555fa5cb50cfd222a8bbe7858fc19f99
AnastasiaDracheva <157720677+AnastasiaDracheva@users.noreply.github.com>
2024-02-29 17:44:13 +0200

    Initial commit with HW7 - Tests

--- src/test/java/org/password/PasswordCheckerTest.java
+++ src/test/java/org/password/PasswordCheckerTest.java
@@ -1,12 +1,44 @@
 package org.password;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class PasswordCheckerTest {
 
     @Test
     void shouldReturnTrueForComplexPasswordWithDigitsAndSpecialChars() {
-         assertTrue( PasswordChecker.checkPasswordComplexity("Password123!") );
+         assertTrue( PasswordChecker.checkPasswordComplexity("Password123!"));
     }
+    @Test
+    void shouldReturnFalseForPasswordWithoutDigits() {
+        assertFalse (PasswordChecker.checkPasswordComplexity("Password"));
+    }
+
+    @Test
+    void shouldReturnFalseForPasswordWithoutSpecialChars() {
+        assertFalse(PasswordChecker.checkPasswordComplexity("Password"));
+    }
+
+    @Test
+    void shouldReturnFalseForPasswordShorterThan8Chars() {
+        assertFalse(PasswordChecker.checkPasswordComplexity("Pass12"));
+    }
+
+    @Test
+    void shouldReturnFalseForEmptyPassword() {
+        assertFalse(PasswordChecker.checkPasswordComplexity("+*^%$*)&($$^**"));
+    }
+
+    @Test
+    void shouldMessageAboutAValidPassword() {
+        assertEquals( PasswordChecker.checkPasswordComplexity("Password123!"),PasswordChecker.checkPasswordComplexity("Password123!"),"Password is complex.");
+
+    }
+    @Test
+    void displayAMessageAboutAnInvalidPassword(){
+        assertEquals( PasswordChecker.checkPasswordComplexity("123!"),PasswordChecker.checkPasswordComplexity("123!"),"Password must contain at least one digit, one special character, and have a length of at least 8 characters.");
+
+    }
+
 }
